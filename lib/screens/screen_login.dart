5ea808e4370490565a1afae37df01dc5446aae7b
violethappyjoy <21bce026@nirmauni.ac.in>
2023-01-20 23:11:50 +0530

    second extended commit!!!

--- lib/screens/screen_login.dart
+++ lib/screens/screen_login.dart
@@ -21,61 +21,87 @@ class _LoginState extends State<LoginPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children:  [
-            const Text(
-              'Login',
-              style: TextStyle(
-                fontSize: 35,
-                color: Colors.deepPurple,
-              ),
-            ),
+            // const Text(
+            //   'Login',
+            //   style: TextStyle(
+            //     fontSize: 35,
+            //     color: Colors.deepPurple,
+            //   ),
+            // ),
 
             Form(
               child: Column(
                 children: [
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      filled: true,
-                      labelText: 'Email',
-                      hintText: 'Enter Email',
-                      prefixIcon: Icon(Icons.email),
-                      prefixIconColor: Colors.deepPurple,
-                      fillColor: Colors.white,
-                      hoverColor: Colors.white,
-                      focusColor: Colors.white,
-                      border: OutlineInputBorder(),
-                    ),
-                    onChanged: (String value){
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        labelText: 'Email',
+                        hintText: 'Enter Email',
+                        prefixIcon: Icon(Icons.email),
+                        prefixIconColor: Colors.deepPurple,
+                        fillColor: Colors.white,
+                        hoverColor: Colors.white,
+                        focusColor: Colors.white,
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value){
 
-                    },
-                    validator: (value){
-                      return value!.isEmpty?'Please enter email': null;
-                    },
+                      },
+                      validator: (value){
+                        return value!.isEmpty?'Please enter email': null;
+                      },
+                    ),
                   ),
 
                   const SizedBox(height: 30,),
 
-                  TextFormField(
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: const InputDecoration(
-                      filled: true,
-                      labelText: 'Password',
-                      hintText: 'Enter Password',
-                      prefixIcon: Icon(Icons.password),
-                      prefixIconColor: Colors.deepPurple,
-                      fillColor: Colors.white,
-                      hoverColor: Colors.white,
-                      focusColor: Colors.white,
-                      border: OutlineInputBorder(),
-                    ),
-                    onChanged: (String value){
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        labelText: 'Password',
+                        hintText: 'Enter Password',
+                        prefixIcon: Icon(Icons.password),
+                        prefixIconColor: Colors.deepPurple,
+                        fillColor: Colors.white,
+                        hoverColor: Colors.white,
+                        focusColor: Colors.white,
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value){
 
-                    },
-                    validator: (value){
-                      return value!.isEmpty?'Please enter password': null;
-                    },
+                      },
+                      validator: (value){
+                        return value!.isEmpty?'Please enter password': null;
+                      },
+                    ),
                   ),
                 ],
+              ),
+            ),
+
+            const SizedBox(height: 30,),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35),
+              child: MaterialButton(
+                minWidth: double.infinity,
+                onPressed: (){
+                  debugPrint('login pressed');
+                },
+                color: Colors.deepPurple,
+                child: const Text(
+                  'login',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             )
           ],
